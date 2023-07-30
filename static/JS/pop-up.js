2e18fbbaba569d5ff4b64750a3737ee1a390a6c1
Mohammad Amin Barzegar <aminbarzegar335@gmail.com>
2023-07-30 15:48:52 +0330

    - creat Education-page HTML file in templates directory

---
+++ static/JS/pop-up.js
@@ -0,0 +1,14 @@
+const popup = document.getElementById("Login-popup-container")
+
+function openPopup() {
+    popup.classList.remove("hidden")
+}
+
+function closePopup(params) {
+    params.preventDefault()
+
+    console.log(params.target)
+    if (params.target.id === "Login-popup-container" || params.target.id === "enter-btn") {
+        popup.classList.add("hidden")
+    }
+}
