5e0f6ea45d26e9f7d7a6cf9ed7dab86580b52a8c
Farzin <farzin.ghafari0000@gmail.com>
2023-08-05 13:11:51 +0330

    x

--- roomzi/urls.py
+++ roomzi/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
+    path('about-us/', views.about_us),
     path('admin/', admin.site.urls),
 ]
