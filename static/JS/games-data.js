9587360e240911d4c3b33d37bc5a13442b464c83
matinisnotdefined <matin.farzamnia85@gmail.com>
2023-08-13 18:15:12 +0430

    - Developing Games-data.

--- static/JS/games-data.js
+++ static/JS/games-data.js
@@ -4,6 +4,12 @@ const gamesDataToggles = document.getElementById("games-data-toggles");
 gamesDataToggles.addEventListener("click", (e) => {
     if (e.target.tagName === "LI") {
         const target = document.querySelector(e.target.dataset.targetId);
+        const toggles = document.querySelectorAll("#games-data-toggles > li");
+
+        toggles.forEach((toggle) => {
+            if (toggle === e.target) toggle.className = "mx-3 cursor-pointer border-b-2 border-b-inherit drop-shadow-[0_0_3px_#F00] pb-3 hover:drop-shadow-[0_0_3px_#F00] hover:border-b-inherit";
+            else toggle.className = "mx-3 cursor-pointer border-b-2 border-b-transparent pb-3 hover:drop-shadow-[0_0_3px_#F00] hover:border-b-inherit";
+        })
 
         gamesDataChilds.forEach((child) => {
             if (child.id !== target.id) child.classList.add("hidden");
