35989bd3a56885bc7fe7b542962571b073067583
Mmd <mashdi.mmd@gmail.com>
2023-08-13 14:19:35 +0330

    -created BaseModel.

--- roomzi/models.py
+++ roomzi/models.py
@@ -1,2 +1,6 @@
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
+
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
