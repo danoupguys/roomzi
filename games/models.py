ab175d586acad4ff3270db3902dd4da850f07c0c
Farzin <farzin.ghafari0000@gmail.com>
2023-08-14 13:30:01 +0330

    - create model games and add class in BaseModel and add database in settings .

--- games/models.py
+++ games/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from roomzi.models import BaseModel
+
 
 # Create your models here.
+class Games(BaseModel):
+    id = models.IntegerField(primary_key=True)
+    name = models.TextField()
+    company_id = models.IntegerField()
+    min_user = models.IntegerField()
+    max_user = models.IntegerField()
+    min_age = models.IntegerField()
+    category = models.TextField()
+    description = models.TextField()
+    price = models.IntegerField()
+    quantity = models.IntegerField()
+    picture = models.TextField()
+    rate = models.IntegerField()
+
+
+class Company(BaseModel):
+    id = models.IntegerField(primary_key=True)
+    name = models.TextField()
+    description = models.TextField()
+    logo = models.TextField()
+
+
+class Category(BaseModel):
+    id = models.IntegerField(primary_key=True)
+    name = models.TextField()
+    description = models.TextField()
--- roomzi/settings.py
+++ roomzi/settings.py
@@ -77,9 +77,13 @@ WSGI_APPLICATION = 'roomzi.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "db_roomzi",
+        "USER": "postgres",
+        "PASSWORD": "Farzin_1384",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
