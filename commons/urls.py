e9b72d82df06fab16e3f3ff49a3048315ce6165b
Farzin <farzin.ghafari0000@gmail.com>
2023-08-15 01:43:00 +0330

    - add function signupForm and loginForm in commons/views .

--- commons/urls.py
+++ commons/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home_page),
+    path("", views.login_page),
     path("about-us/", views.about_us, name="about_us"),
 ]
--- commons/views.py
+++ commons/views.py
@@ -1,9 +1,27 @@
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.shortcuts import render
 
 
 # Create your views here.
 def home_page(request):
-    return render(request, 'commons/index.html')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = UserCreationForm()
+    return render(request, 'commons/index.html', {'signup': form})
+
+
+def login_page(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            login(request, user=form.get_user())
+    else:
+        form = AuthenticationForm()
+    return render(request, 'commons/index.html', {'login': form})
 
 
 def about_us(request):
