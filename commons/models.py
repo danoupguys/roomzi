d039a4a43f52427727d24dfb8e855f14ca4f96fe
Mmd <mashdi.mmd@gmail.com>
2023-08-20 14:43:03 +0330

    - Edit name class .

--- commons/models.py
+++ commons/models.py
@@ -2,17 +2,17 @@ from django.db import models
 from roomzi.models import BaseModel
 
 
-class About_us(BaseModel):
+class AboutUs(BaseModel):
     description = models.TextField()
 
 
-class Social_media(BaseModel):
+class SocialMedia(BaseModel):
     title = models.TextField()
     link = models.TextField()
     icon = models.TextField()
 
 
-class Team_members(BaseModel):
+class TeamMembers(BaseModel):
     Member_Choices = (
         (1, "مدیر"),
         (2, "برنامه نویس فرانت"),
