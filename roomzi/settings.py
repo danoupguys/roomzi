d1ba7ff9071dbc17efadfe016b8d7732e48b71dd
Mmd <mashdi.mmd@gmail.com>
2023-08-21 10:26:04 +0330

    - add todo comment .

--- roomzi/settings.py
+++ roomzi/settings.py
@@ -100,16 +100,16 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    # },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    # },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    # },
+]  # TODO => uncomment validators on production.
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
