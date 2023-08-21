112052d052273d2df53d58ad400e24bdf132684e
Farzin <farzin.ghafari0000@gmail.com>
2023-08-22 00:08:00 +0330

    - add events. games. about us to jango admin .

---
+++ commons/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from commons.models import AboutUs, SocialMedia, TeamMembers
+admin.site.register(AboutUs)
+admin.site.register(SocialMedia)
+admin.site.register(TeamMembers)
--- games/admin.py
+++ games/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from games.models import Games, Company, Category
+admin.site.register(Games)
+admin.site.register(Company)
+admin.site.register(Category)
 
-# Register your models here.
